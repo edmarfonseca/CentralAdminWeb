7197cf848bcdbd399b0d4b34ae7b629759e486cb
edmarfonseca <edmarfonseca12@gmail.com>
2025-02-02 02:32:29 -0300

    atualização do navbar

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+//import * as CryptoJS from 'crypto-js';
 import { environment } from '../../../../environments/environment';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   imports: [
-    CommonModule,
-    RouterLink
+    CommonModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
@@ -15,6 +14,8 @@ import { CommonModule } from '@angular/common';
 export class NavbarComponent {
 
   nomeUsuario: string = '';
+  iniciais: string = '';
+  menuAberto: boolean = false;
   isAuthenticated: boolean = false;
 
   ngOnInit() {
@@ -25,10 +26,18 @@ export class NavbarComponent {
       this.isAuthenticated = true;
 
       //const usuario = JSON.parse(CryptoJS.AES.decrypt(data, environment.cryptoKey).toString(CryptoJS.enc.Utf8));
-      const usuario = JSON.parse(data).toString();
+      const usuario = JSON.parse(data);     
 
       this.nomeUsuario = usuario.nome;
+
+      this.iniciais = this.getIniciais(this.nomeUsuario);
     }
+  }
+
+  getIniciais(nome: string): string {
+    
+    const partes = nome.trim().split(' ');
+    return partes.length === 1 ? partes[0][0] : (partes[0][0] + partes[1][0]).toUpperCase();
   }
 
   logout() {
